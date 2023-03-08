bb23d21b892885e9ea352a6b5a1406bce0d590c4
kimhyunkyung <osanaihyun@gmail.com>
2023-03-08 14:45:37 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+class Todo {
+  bool isDone = false;
+  String title;
+
+  Todo(this.title);
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,11 +16,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoListPage(),
     );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final _items = [];
+  final _todoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('남은 할  일'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Expanded(
+                    child: TextField(),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    child: const Text(
+                      'add',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  children: const [],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
