d60444da5aafca615f19e4a1485aae16a5ce874d
kimhyunkyung <osanaihyun@gmail.com>
2024-01-12 20:46:08 +0900

    plus

---
+++ MyTodo/flutter_mytodolist_1/android/app/src/main/kotlin/com/example/flutter_mytodolist_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_mytodolist_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/todolist01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.todolist01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
